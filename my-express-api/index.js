48018af3fad7a4b8c10642c94fd38c374336de6b
Juan-Pablo-Barrientos <jpbarrientosros@outlook.com>
2024-05-08 14:59:43 -0300

    fix

--- my-express-api/index.js
+++ my-express-api/index.js
@@ -1,5 +1,5 @@
 const express = require("express"); const app = express(); 
-app.get("/", (req, res) => { res.send("Express on Vercel"); });
+app.get("/", (req, res) => { res.send(process.env.CLIENT_KEY); });
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 module.exports = app;
